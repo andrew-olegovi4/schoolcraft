6cc53d5a5326e5d6b551eb1fbb4fb72046dac7b7
joseanastacio <joseanastaciocastilho@gmail.com>
2022-04-14 19:00:49 -0300

    Add files via upload
    I added mod files

--- init.lua
+++ init.lua
@@ -1,4 +1,5 @@
 school_furniture = {}
 dofile(minetest.get_modpath("school_furniture") .. DIR_DELIM .. "furniture.lua")
+dofile(minetest.get_modpath("school_furniture") .. DIR_DELIM .. "canteen.lua")	
+
 dofile(minetest.get_modpath("school_furniture") .. DIR_DELIM .. "schooldrawings/colorful_drawings.lua")
-	
